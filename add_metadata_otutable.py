44923b8cf12120245487f6954266962b40d75181
MartenHoogeveen <martenhoogeveen@gmail.com>
2018-11-12 13:43:50 +0100

    Added metadata options

--- add_metadata_otutable.py
+++ add_metadata_otutable.py
@@ -12,6 +12,7 @@ requiredArguments.add_argument('-io', '--input-otutable', dest='otutable', type=
 requiredArguments.add_argument('-o', '--output', dest='output', type=str, required=True)
 requiredArguments.add_argument('-t', '--input-type', metavar='metadata type (Blast or LCA input)', dest='type', type=str,
                                required=True, choices=['blast', 'lca','automatic'])
+requiredArguments.add_argument('-n', dest='naValues', type=str, required=True)
 args = parser.parse_args()
 
 def metadata_to_dict():
@@ -35,37 +36,57 @@ def detect_input(metadataHeader):
     """
     Makes the life of the user a little easier, it saves an extra click in galaxy.
     """
+    metadataHeaderList = metadataHeader.split("\t")
     if args.type == "automatic":
         if "#Identity percentage" in metadataHeader:
             return "blast"
         elif "#lca rank" in metadataHeader:
             return "lca"
-    elif args.type == "blast":
-        return "blast"
-    elif args.type == "lca":
-        return "lca"
+        elif "#Frame 1 stop codons" in metadataHeaderList and "#Frame 1" in metadataHeaderList:
+            return "translation6andstopcodons"
+        elif "#Frame 1" not in metadataHeaderList and "#Frame 1 stop codons" in metadataHeaderList:
+            return "stopcodons"
+        elif "#Frame 1" in metadataHeaderList and "#Frame 1 stop codons" not in metadataHeaderList:
+            return "translation6"
+    else:
+        return str(args.type)
 
-def add_blast_or_lca_to_otutable(metadataDict, metadataHeader, inputType):
+
+def add_general_to_otutable(metadataDict, metadataHeader, inputType):
     with open(args.output, "a") as output, open(args.otutable, "r") as otutable:
         output.write(otutable.readline().strip()+"\t"+metadataHeader+"\n")
         for line in otutable:
             if line.split("\t")[0] in metadataDict:
                 output.write(line.strip()+"\t"+metadataDict[line.split("\t")[0]])
             else:
+                if args.naValues == "yes":
+                    emptyValue = "\tN/A"
+                else:
+                    emptyValue = "\t"
+
                 if inputType == "blast":
-                    naString = 8*"\tN/A"
+                    naString = 8*emptyValue
                     output.write(line.strip() +"\t"+line.split("\t")[0]+naString+"\tunknown / unknown / unknown / unknown / unknown / unknown / unknown\n")
                 elif inputType == "lca":
-                    naString = 10*"\tN/A"
+                    naString = 10*emptyValue
+                    output.write(line.strip() +"\t"+line.split("\t")[0]+naString+"\n")
+                elif inputType == "translation6":
+                    naString = 6*emptyValue
+                    output.write(line.strip() +"\t"+line.split("\t")[0]+naString+"\n")
+                elif inputType == "translation6andstopcodons":
+                    naString = 12*emptyValue
+                    output.write(line.strip() +"\t"+line.split("\t")[0]+naString+"\n")
+                elif inputType == "stopcodons":
+                    naString = 6*emptyValue
                     output.write(line.strip() +"\t"+line.split("\t")[0]+naString+"\n")
 
 def main():
     metadataDict = metadata_to_dict()
     metadataHeader = get_metadata_header()
     inputType = detect_input(metadataHeader)
-
-    if inputType == "blast" or inputType == "lca":
-        add_blast_or_lca_to_otutable(metadataDict, metadataHeader, inputType)
+    inputTypes = ["blast", "lca", "translation6", "translation6andstopcodons", "stopcodons"]
+    if inputType in inputTypes:
+        add_general_to_otutable(metadataDict, metadataHeader, inputType)
     if inputType == "other":#option to expand this tool
         pass
 
--- add_metadata_otutable.sh
+++ add_metadata_otutable.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-add_metadata_otutable.py -im $1 -io $2 -o $3 -t $4
+add_metadata_otutable.py -im $1 -io $2 -o $3 -t $4 -n $5
