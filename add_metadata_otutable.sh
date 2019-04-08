05f0ee2d9cff44af26e0d5c45c81a572ddbed968
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-04-08 15:09:18 +0000

    update

--- add_metadata_otutable.sh
+++ add_metadata_otutable.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-add_metadata_otutable.py -im $1 -io $2 -o $3 -t $4 -n $5
+SCRIPTDIR=$(dirname "$(readlink -f "$0")")
+python $SCRIPTDIR"/add_metadata_otutable.py" -im $1 -io $2 -o $3 -t $4 -n $5
